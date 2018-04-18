1ec53b3ed613a91e503b07f61bcb8240420dcc74
Isaac Hailperin <isaac.hailperin@gmail.com>
2018-04-18 10:59:50 +0200

    using custom weblogs

--- simple_dist2/create_logs1.sh
+++ simple_dist2/create_logs1.sh
@@ -3,7 +3,7 @@ trap 'kill $(jobs -p)' SIGINT
 # generate logs for three hosts
 for i in 1 2 3; do
 	# weblogs
-	./gogen -c gogens/weblog.yml -o file -f /opt/log/www${i}/access.log gen -r -c 1 -i 10 &
+	./gogen -c mygogens/myweblog.yml -o file -f /opt/log/www${i}/access.log gen -r -c 1 -i 10 &
 	# secure logs
 	./gogen -c mygogens/securelog${i}.yml -o file -f /opt/log/www${i}/secure.log &
 done
